d4beb66c2ef3b264ccdbfe6512a055661338a397
Hubert Bereś <hubert@springbok.ai>
2024-03-23 21:22:09 +0000

    Get the calculator to work

--- src/pages/AzurePriceCalculator.jsx
+++ src/pages/AzurePriceCalculator.jsx
@@ -1,48 +1,59 @@
-import React, { useState } from 'react';
-import { useQuery, useAction, createCostEstimate, getCostEstimate } from 'wasp/client/operations';
+import { useState } from 'react';
 
 const AzurePriceCalculatorPage = () => {
-  const { data: costEstimate, isLoading, error } = useQuery(getCostEstimate);
-  const createCostEstimateFn = useAction(createCostEstimate);
+  let [azureCost, setAzureCost] = useState(null);
   const [awsCost, setAwsCost] = useState(0);
   const [isEnterprise, setIsEnterprise] = useState(false);
 
   const handleFormSubmit = () => {
-    createCostEstimateFn({ awsCost, isEnterprise });
+    if (isEnterprise)
+      setAzureCost(9 * awsCost);
+    else
+      setAzureCost(3 * awsCost);
   };
 
   return (
     <div className='p-4'>
-      <h1 className='text-3xl font-bold'>Azure Price Calculator</h1>
       <p className='text-gray-500'>Estimate your infrastructure costs when migrating to Azure.</p>
-      <form onSubmit={handleFormSubmit} className='mt-4'>
+      <h3 style={{padding: '1rem'}}>Existing costs</h3>
+        <label className='ml-2 text-lg'
+        style={{ padding: '1rem', marginRight: '1rem'}}
+        >
+        AWS cost ($/month):
         <input
           type='number'
           placeholder='AWS Cost ($/month)'
           className='px-1 py-2 border rounded text-lg'
           value={awsCost}
           onChange={(e) => setAwsCost(e.target.value)}
-        />
-        <label className='ml-2 text-lg'>
+          />
+          </label>
+        <br></br>
+        <h3 style={{padding: '1rem'}}>Additional requirements:</h3>
+        <label className='ml-2 text-lg' style={{ padding: '1rem', marginRight: '1rem'}}>
           <input
             type='checkbox'
             checked={isEnterprise}
             onChange={() => setIsEnterprise(!isEnterprise)}
+            style = {{ marginRight: '0.5rem'}}
           />
-          Is Enterprise
+           I need the 'Enterprise' stuff
         </label>
+        <br style={{ padding: '2rem'}}></br>
         <button
-          type='submit'
+          type='button'
           className='bg-blue-500 hover:bg-blue-700 px-4 py-2 text-white font-bold rounded mt-2'
+          style={{ cursor: 'pointer', padding: '1rem' }}
+          onClick={handleFormSubmit}
         >
-          Calculate
+          Get my estimate!
         </button>
-      </form>
-      {costEstimate && (
+      <br></br>
+      {azureCost !== null  && (
         <div className='mt-4'>
-          <p>Estimated Azure Cost:</p>
+          <p>Cost after migration to Azure:</p>
           <p className='text-xl font-bold'>
-            {isEnterprise ? awsCost * 9 : awsCost * 3}
+          ${azureCost}/month
           </p>
         </div>
       )}
